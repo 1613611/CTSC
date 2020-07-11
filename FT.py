ee549feb93060c87feaa69905279cfa43c8fb76b
viettoantran98 <viettoantran98@gmail.com>
2020-07-11 12:00:17 +0700

    FT control and log vehicle information

--- FT.py
+++ FT.py
@@ -13,7 +13,6 @@ import traci
 parser = argparse.ArgumentParser() 
 parser.add_argument("--gui", action='store_true', help = "Enable gui")
 parser.add_argument('--net-file', type=str, dest="net_file", help='Name of the net file')
-parser.add_argument('--route-file', type=str, dest="route_file", help='Name of the route file')
 parser.add_argument("--light-traffic", action='store_true', dest="light_traffic", default=False, help = "Use workload of light traffic") 
 parser.add_argument("--heavy-traffic", action='store_true', dest="heavy_traffic", default=False, help = "Use workload of heavy traffic") 
 args = parser.parse_args()
@@ -32,13 +31,16 @@ if args.net_file == '4-arterial-intersections':
                             '2to3_0', 'NtoC_3_0', '4to3_0', 'StoC_3_0', '3to4_0', 'NtoC_4_0', '5to4_0', 'StoC_4_0',\
                             '0to1_1', 'NtoC_1_1', '2to1_1', 'StoC_1_1', '1to2_1', 'NtoC_2_1', '3to2_1', 'StoC_2_1',\
                             '2to3_1', 'NtoC_3_1', '4to3_1', 'StoC_3_1', '3to4_1', 'NtoC_4_1', '5to4_1', 'StoC_4_1']
-    
+    DISTANCE_OF_ROUTE = {"route1": 830, "route2": 830, "route1A": 320, "route1B": 320, "route2A": 320, "route2B": 320, \
+                         "route3A": 320, "route3B": 320, "route4A": 320, "route4B": 320}    
 if args.heavy_traffic:
-    sumoCmd.extend(['-r', './network/%s.heavy.route.xml' % args.route_file])
-    LOG_FILE_NAME = './log/FT-queue-length-%s-heavy-traffic.txt' % args.net_file
+    sumoCmd.extend(['-r', './network/%s.heavy.route.xml' % args.net_file])
+    LOG_QUEUE_LENGTH_FILE_NAME = './log/FT-queue-length-%s-heavy-traffic.txt' % args.net_file
+    LOG_VEHICLE_FILE_NAME = './log/FT-vehicle-%s-heavy-traffic.txt' % args.net_file
 elif args.light_traffic:
-    sumoCmd.extend(['-r', './network/%s.light.route.xml' % args.route_file])
-    LOG_FILE_NAME = './log/FT-queue-length-%s-light-traffic.txt' % args.net_file
+    sumoCmd.extend(['-r', './network/%s.light.route.xml' % args.net_file])
+    LOG_QUEUE_LENGTH_FILE_NAME = './log/FT-queue-length-%s-light-traffic.txt' % args.net_file
+    LOG_VEHICLE_FILE_NAME = './log/FT-vehicle-%s-light-traffic.txt' % args.net_file
 
 TRAFFIC_SIGNAL_LIGHT_NAMES = ['node1', 'node2', 'node3', 'node4']
 MOVEMENT_DISTANCE = [150*5 + 4*20, 150*2 + 20]
@@ -50,22 +52,45 @@ class Simulation_FT():
         traci.start(sumoCmd)
         self.current_phase_duration = 0
         self.queue_length_per_step = []
-        self.log_file = open(LOG_FILE_NAME, "w")
-        self.log_file.write('STEP,W11,W12,N11,N12,E11,E12,S11,S12,W21,W22,N21,N22,E21,E22,S21,S22,W31,W32,N31,N32,E31,E32,S31,S32,W41,W42,N41,N42,E41,E42,S41,S42\n')
+        self.log_QL_file = open(LOG_QUEUE_LENGTH_FILE_NAME, "w")
+        if args.net_file == '4-arterial-intersections':
+            self.log_QL_file.write('STEP,W11,W12,N11,N12,E11,E12,S11,S12,W21,W22,N21,N22,E21,E22,S21,S22,W31,W32,N31,N32,E31,E32,S31,S32,W41,W42,N41,N42,E41,E42,S41,S42\n')
+
+        self.log_Veh_file = open(LOG_VEHICLE_FILE_NAME, "w")
+        self.log_Veh_file.write('VehID,DepartedTime,ArrivedTime,RouteID,TravelTime,AverageSpeed\n')            
+        self.vehicle_tracker = dict()
 
     def nextStep(self):
+        self.log_step()
+        self.current_phase_duration += 1
+        traci.simulationStep()
+
+    def log_step(self):
+        # QUEUE_LENGTH
         q_length = []
         for lane in LIST_INCOMING_LANES:
             q_length.append(traci.lane.getLastStepHaltingNumber(lane))
         str_q_length = str(traci.simulation.getTime())
         for q in q_length:
             str_q_length += ',%d' % q
-        self.log_file.write(str_q_length + '\n')
+        self.log_QL_file.write(str_q_length + '\n')
         self.queue_length_per_step.append(q_length)
 
-        self.current_phase_duration += 1
-        traci.simulationStep()
-
+        # LOG VEHICLE INFORMATION
+        step = traci.simulation.getTime()
+        for vehicleId in traci.simulation.getDepartedIDList():
+            self.vehicle_tracker[vehicleId] = dict()
+            self.vehicle_tracker[vehicleId]['departed'] = step
+            self.vehicle_tracker[vehicleId]['routeID'] = traci.vehicle.getRouteID(vehicleId)
+        for vehicleId in traci.simulation.getArrivedIDList():
+            self.vehicle_tracker[vehicleId]['arrived'] = step
+            self.vehicle_tracker[vehicleId]['travel_time'] = self.vehicle_tracker[vehicleId]['arrived'] - self.vehicle_tracker[vehicleId]['departed']
+            self.vehicle_tracker[vehicleId]['average_speed'] = DISTANCE_OF_ROUTE[self.vehicle_tracker[vehicleId]['routeID']]/self.vehicle_tracker[vehicleId]['travel_time']
+            self.log_Veh_file.write('%s,%f,%f,%s,%f,%f\n' % (vehicleId,   self.vehicle_tracker[vehicleId]['departed'],\
+                                                                        self.vehicle_tracker[vehicleId]['arrived'],\
+                                                                        self.vehicle_tracker[vehicleId]['routeID'],\
+                                                                        self.vehicle_tracker[vehicleId]['travel_time'],\
+                                                                        self.vehicle_tracker[vehicleId]['average_speed']))
     def changePhase(self):
         self.current_phase_duration = 0
         currentPhase = traci.trafficlight.getPhase(TRAFFIC_SIGNAL_LIGHT_NAMES[0])
@@ -89,7 +114,6 @@ class Simulation_FT():
     def isFinised(self):
         if traci.simulation.getMinExpectedNumber() <= 0:
             traci.close()
-            print(np.mean(self.queue_length_per_step))
             return True
         return False
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # multi-agent-TSC
-
-python FT.py --net-file 4-arterial-intersections --route-file 4-arterial-intersections --light-traffic --gui
+Use the fixed-time control as the bellow command:
+> python FT.py --net-file 4-arterial-intersections --light-traffic --gui
+Where:
+- **--net-file net_file** to select the test site
+- **--light-traffic** or **--heavy-traffic** to select the kind of traffic, the default traffic is light traffic
+- **--gui** to enable GUI
